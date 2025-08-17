0bb1a0fece7cf4ee0a684b45542a6b73c7255242
Timur-063 <funniest0017@gmail.com>
2025-08-17 14:11:00 +0500

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,11 +1,14 @@
 from aiogram import Bot, Dispatcher
 from data import config
 from baza.sqlite import Database
+from aiogram.client.session.aiohttp import AiohttpSession
+
+session = AiohttpSession(proxy='http://proxy.server:3128')
 
 ADMINS = config.ADMINS
 TOKEN = config.BOT_TOKEN
 CHANNELS = config.CHANNELS
 
-bot = Bot(TOKEN)
+bot = Bot(TOKEN, session=session)
 db = Database(path_to_db="main.db")
 dp = Dispatcher()
